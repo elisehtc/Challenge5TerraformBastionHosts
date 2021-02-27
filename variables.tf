228dba9bcafa15e5b31cd844e5e9715890903502
elisehtc <57683535+elisehtc@users.noreply.github.com>
2021-02-27 15:59:24 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable region {
+    default="eu-north-1"
+}
+
+variable profile {
+    default="personal"
+}
+
+variable instance-ami {
+    default="ami-02a6bfdcf8224bd77"
+}
+
+variable key_path {
+    default = "keys/mykeypair.pub"
+}
